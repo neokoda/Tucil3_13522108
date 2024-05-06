90f1ed84db331900261a838e8d74e7fd0ba8b705
Neo Koda <mneocicerok@gmail.com>
2024-05-06 15:33:31 +0700

    chore : fix indentation

--- src/WordNode.java
+++ src/WordNode.java
@@ -1,29 +1,29 @@
-    import java.util.ArrayList;
+import java.util.ArrayList;
 
-    public class WordNode implements Comparable<WordNode> {
-        private String word;
-        private ArrayList<String> path;
-        private int cost;
+public class WordNode implements Comparable<WordNode> {
+    private String word;
+    private ArrayList<String> path;
+    private int cost;
 
-        public WordNode(String word, ArrayList<String> path, int cost) {
-            this.word = word;
-            this.path = path;
-            this.cost = cost;
-        }
-
-        public String getWord() {
-            return word;
-        }
-
-        public ArrayList<String> getPath() {
-            return path;
-        }
-
-        public int getCost() {
-            return cost;
-        }
-
-        public int compareTo(WordNode otherWord) {
-            return Integer.compare(this.cost, otherWord.cost);
-        }
+    public WordNode(String word, ArrayList<String> path, int cost) {
+        this.word = word;
+        this.path = path;
+        this.cost = cost;
     }
+
+    public String getWord() {
+        return word;
+    }
+
+    public ArrayList<String> getPath() {
+        return path;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int compareTo(WordNode otherWord) {
+        return Integer.compare(this.cost, otherWord.cost);
+    }
+}
